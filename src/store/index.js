4d141733760f3e1d166edba393b7683b2e305a23
ryoken.miyazaki <ryoken.miyazaki@B436974.local>
2019-04-19 15:21:21 +0900

    edited vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,20 +4,18 @@ import Vue from 'vue'
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
-    modules :{
-      state : {
-          meeting_info : {
-              scheduled_s_time : 1555646400, //2019/04/19 13:00:00
-              scheduled_e_time : 1555650000, //2019/04/19 14:00:00
-              actual_s_time : null,
-              actual_e_time : null,
-              description : "ŠJ”­‰ï‹c",
-          },
-      },
-      mutations : {
+    state : {
+        meeting_info : {
+            scheduled_s_time : 1555646400, //2019/04/19 13:00:00
+            scheduled_e_time : 1555650000, //2019/04/19 14:00:00
+            actual_s_time : null,
+            actual_e_time : null,
+            description : "ï¿½Jï¿½ï¿½ï¿½ï¿½c",
+        },
+    },
+    mutations : {
 
-      },
-    }
+    },
 })
    
 export default store
