cd0f22a9b1f81bc45fb60abb579dff43948ea12b
pakio <kazuma.arimura@zozo.com>
2019-04-19 12:52:36 +0900

    storeに共通モックデータ挿入'

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,16 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
     modules :{
-      //
+      state : {
+          meeting_info : {
+              s_time : 1555646400, //2019/04/19 13:00:00
+              e_time : 1555650000, //2019/04/19 14:00:00
+              description : "ŠJ”­‰ï‹c",
+          },
+      },
+      mutations : {
+
+      },
     }
 })
    
